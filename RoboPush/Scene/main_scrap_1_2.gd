988b5b479de380290398b842116c7b3455b956c6
pflorid2 <pflorid2@illinois.edu>
2023-11-08 21:49:08 -0600

    updated sprites and more cases covered

--- RoboPush/Scene/main_scrap_1_2.gd
+++ RoboPush/Scene/main_scrap_1_2.gd
@@ -11,44 +11,54 @@ var inputs = {
 	'ui_right': Vector2.RIGHT
 }
 
-var ball1_2
-var ball1_3
-var ball1_2_3
+var ball1;
+var ball2;
+var ball3;
+var ball1_2;
+var ball2_3;
+
 func _ready():
+	ball1 = get_parent().get_node("MAIN_Scrap1").duplicate()
+	ball2 = get_parent().get_node("MAIN_Scrap2").duplicate()
 	ball1_2 = get_parent().get_node("MAIN_Scrap1_2").duplicate()
-	ball1_3 = get_parent().get_node("MAIN_Scrap1_3").duplicate()
-	ball1_2_3 = get_parent().get_node("MAIN_Scrap1_2_3").duplicate()
+	ball3 = get_parent().get_node("MAIN_Scrap3").duplicate()
 
 func move(dir):
 	# Calculate vector and do ray stuff
 	var vector_pos = inputs[dir] * grid_size
+	
 	ray.target_position = vector_pos
 	ray.force_raycast_update()
 	
 	# If scrap ball doesn't collide with anything, then it can be pushed
 	if !ray.is_colliding():
-		position += vector_pos
-		return true
-#	# If it does collide with something, make sure it's a bigger ball and you can put it on top
+		get_parent().add_child(ball1)
+		get_parent().add_child(ball2)
+		ball1.position = position + vector_pos
+		ball2.position = position
+		get_parent().remove_child(self)
+		return false
+
+	# If it does collide with something, make sure it's a smaller ball and you can put it on top
 	else:
 		var collider = ray.get_collider()
-
 		if collider.is_in_group('two_balls'):
 			get_parent().add_child(ball1_2)
+			get_parent().add_child(ball2)
 			ball1_2.position = position + vector_pos
+			ball2.position = position
 			get_parent().remove_child(collider)
 			get_parent().remove_child(self)
-		elif collider.is_in_group('three_balls'):
-			get_parent().add_child(ball1_3)
-			ball1_2.position = position + vector_pos
-			get_parent().remove_child(collider)
-			get_parent().remove_child(self)
+			return false
 		elif collider.is_in_group('two_three_balls'):
-			get_parent().add_child(ball1_2_3)
-			ball1_2_3.position = position + vector_pos
+			get_parent().add_child(ball1_2)
+			get_parent().add_child(ball2)
+			ball1_2.position = position + vector_pos
+			ball2.position = position
 			get_parent().remove_child(collider)
 			get_parent().remove_child(self)
-			return true
+			return false
+			
 	return false
 
 	
